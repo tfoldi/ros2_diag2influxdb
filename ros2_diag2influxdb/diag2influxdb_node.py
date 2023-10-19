eacf09fea532a59d379cb58f7ddcaee05fb72947
Földi Tamás <tfoldi@xsi.hu>
2023-10-19 19:08:30 +0200

    feat: store diagnostics data to influxdb

--- ros2_diag2influxdb/diag2influxdb_node.py
+++ ros2_diag2influxdb/diag2influxdb_node.py
@@ -41,6 +41,12 @@ class Diag2InfluxdbNode(Node):
         qos_history_desc = ParameterDescriptor(description="QoS history depth")
         self.declare_parameter("qos_history", 100, qos_history_desc)
 
+        self.declare_parameter("influx_bucket", "default")
+        self.declare_parameter("influx_token", "")
+        self.declare_parameter("influx_url", "http://influxdb:8086")
+        self.declare_parameter("influx_org_id", "")
+        self.declare_parameter("influx_measurement", "diagnostics")
+
         qos_history = (
             self.get_parameter("qos_history").get_parameter_value().integer_value
         )
@@ -53,10 +59,17 @@ class Diag2InfluxdbNode(Node):
         )
         self.subscription  # prevent unused variable warning
 
-        self.influx_bucket = "antares"
-        token = "3f1cZRrR4mMOacWR5ManKYCKfFEyQqNf"
-        influx_url = "http://127.1.27.3"
-        self.org_id = "e6cd7430a342269a"
+        self.influx_bucket = (
+            self.get_parameter("influx_bucket").get_parameter_value().string_value
+        )
+        token = self.get_parameter("influx_token").get_parameter_value().string_value
+        influx_url = self.get_parameter("influx_url").get_parameter_value().string_value
+        self.org_id = (
+            self.get_parameter("influx_org_id").get_parameter_value().string_value
+        )
+        self.measurement = (
+            self.get_parameter("influx_measurement").get_parameter_value().string_value
+        )
 
         self.influx_client = InfluxDBClient(
             url=influx_url, token=token, org=self.org_id, debug=False
@@ -64,21 +77,27 @@ class Diag2InfluxdbNode(Node):
 
         self.messages = []
 
-    def listener_callback(self, msg):
-        self.get_logger().debug('received message: "%s/%s"' % (msg.id, hex(msg.id)))
+    def return_val(s):
+        try:
+            return int(s)
+        except ValueError:
+            pass
 
-        message = {
-            "measurement": "Dodgem",
-            "fields": {
-                k: str(v)
-                if isinstance(v, cantools.database.namedsignalvalue.NamedSignalValue)
-                else v
-                for k, v in val.items()
-            },
-            # "time": time.time() * 1000 * 1000000,
-        }
+        try:
+            return float(s)
+        except ValueError:
+            pass
 
-        self.messages.append(message)
+        return s
+
+    def listener_callback(self, val):
+        for status in val.status:
+            message = {
+                "measurement": self.measurement,
+                "fields": {status.items()},
+                # "time": time.time() * 1000 * 1000000,
+            }
+            self.messages.append(message)
 
         if len(self.messages) > 2000:
             with self.influx_client.write_api(
@@ -94,23 +113,6 @@ class Diag2InfluxdbNode(Node):
                     self.influx_bucket, self.org_id, record=self.messages
                 )
                 self.messages = []
-
-        # self.messages.append(message)
-
-        # if len(self.messages) > 2000:
-        #     with self.influx_client.write_api(
-        #         write_options=WriteOptions(
-        #             batch_size=2000,
-        #             flush_interval=1000,
-        #             jitter_interval=0,
-        #             retry_interval=5_000,
-        #             max_retries=0,
-        #         )
-        #     ) as _write_client:
-        #         _write_client.write(
-        #             self.influx_bucket, self.org_id, record=self.messages
-        #         )
-        #         self.messages = []
 
 
 def main(args=None):
