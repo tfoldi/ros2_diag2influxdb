2697270708fe5d6de62e46faaa9559255b2bab42
Földi Tamás <tfoldi@xsi.hu>
2023-10-20 19:44:12 +0200

    style: black format

--- setup.py
+++ setup.py
@@ -1,26 +1,25 @@
 from setuptools import setup
 
-package_name = 'ros2_diag2influxdb'
+package_name = "ros2_diag2influxdb"
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools','influxdb_client'],
+    install_requires=["setuptools", "influxdb_client"],
     zip_safe=True,
-    maintainer='tfoldi',
-    maintainer_email='tfoldi@xsi.hu',
-    description='Send data from diagnostics topic to InfluxDB',
-    license='BSD-3-Clause',
-    tests_require=['pytest'],
+    maintainer="tfoldi",
+    maintainer_email="tfoldi@xsi.hu",
+    description="Send data from diagnostics topic to InfluxDB",
+    license="BSD-3-Clause",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'diag2influxdb_node = ros2_diag2influxdb.diag2influxdb_node:main'
+        "console_scripts": [
+            "diag2influxdb_node = ros2_diag2influxdb.diag2influxdb_node:main"
         ],
     },
 )
