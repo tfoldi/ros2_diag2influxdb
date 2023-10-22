081ea50befd56514caa847f307b69bfaf33e7d2a
Földi Tamás <tfoldi@xsi.hu>
2023-10-22 12:39:02 +0200

    chore: log level parameter

--- launch/from_candecode.py
+++ launch/from_candecode.py
@@ -1,8 +1,10 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
 from launch_ros.substitutions import FindPackageShare
-from launch.substitutions import PathJoinSubstitution
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 
+logger = LaunchConfiguration("log-level")
 
 decode_config_file = PathJoinSubstitution(
     [FindPackageShare("ros2_candecode"), "config", "candecode.yaml"]
@@ -16,13 +18,18 @@ antares_config_file = PathJoinSubstitution(
 def generate_launch_description():
     return LaunchDescription(
         [
+            DeclareLaunchArgument(
+                "log-level",
+                default_value=["debug"],
+                description="Logging level",
+            ),
             Node(
                 package="ros2_candecode",
                 executable="candecode",
                 name="candecode_node",
                 output="log",
                 parameters=[decode_config_file],
-                arguments=['--ros-args', '--log-level', 'debug']
+                arguments=['--ros-args', '--log-level', logger]
             ),
             Node(
                 package="ros2_diag2influxdb",
@@ -30,7 +37,7 @@ def generate_launch_description():
                 name="diag2influxdb_node",
                 output="log",
                 parameters=[antares_config_file],
-                arguments=['--ros-args', '--log-level', 'debug']
+                arguments=['--ros-args', '--log-level', logger]
             )
         ]
     )
